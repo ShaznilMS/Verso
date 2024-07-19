ab72fb14d1e6b4b9a0200499fa3673c0148dd11a
Antonio Cossa <antoniovcossa16@gmail.com>
2024-07-19 10:52:05 +0200

    Tab Bar Completa

--- app/Views/Home/index.jsx
+++ app/Views/Home/index.jsx
@@ -2,20 +2,76 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { Views } from '../../../configs/views.config.mjs';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faBookmark, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { View } from 'react-native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 const Tab = createBottomTabNavigator();
+
+const ScreenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyle: {
+        position: 'absolute',
+        bottom: 0,
+        rigth: 0,
+        left: 0,
+        elevation: 0,
+        height: 80,
+        padding: 30
+    }
+}
+
+const focusedTab = {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    backgroundColor: '#bebec0',
+    borderRadius: 20
+}
 
 export default function HomeConfigs() {
     return (
         <NavigationContainer independent={true}>
             <Tab.Navigator
+                screenOptions={ScreenOptions}
                 initialRouteName='Home'
             >
-                <Tab.Screen name='Home' component={Views.Home} options={{}}/>
-                <Tab.Screen name='Profile' component={Views.Profile}  options={{}}/>
-                <Tab.Screen name='Favorite' component={Views.Favorite}  options={{}}/>
-                <Tab.Screen name='Notifications' component={Views.Notifications}  options={{}}/>
+                <Tab.Screen name='Home' component={Views.Home} options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View style={focused ? { display: 'flex' } : focusedTab}>
+                                <FontAwesomeIcon icon={faHome} size={23} color={'#49454F'} />
+                            </View>
+                        )
+                    }
+                }} />
+                <Tab.Screen name='Profile' component={Views.Profile} options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View style={focused ? focusedTab : { display: 'flex' }}>
+                                <FontAwesomeIcon icon={faUser} size={23} color={'#49454F'} />
+                            </View>
+                        )
+                    }
+                }} />
+                <Tab.Screen name='Favorite' component={Views.Favorite} options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View style={focused ? focusedTab : { display: 'flex' }}>
+                                <FontAwesomeIcon icon={faBookmark} size={23} color={'#49454F'} />
+                            </View>
+                        )
+                    }
+                }} />
+                <Tab.Screen name='Notifications' component={Views.Notifications} options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View style={focused ? focusedTab : { display: 'flex' }}>
+                                <FontAwesomeIcon icon={faBell} size={23} color={'#49454F'} />
+                            </View>
+                        )
+                    }
+                }} />
             </Tab.Navigator>
         </NavigationContainer>
     );
