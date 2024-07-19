c6e1c70fd768818ce3bf9006dc67f0675a8296b0
ShaznilMS <shaznilmussagyoficial@gmail.com>
2024-07-19 18:39:31 +0100

    update app/Views/Home/Home/index.jsx

--- app/Views/Home/Home/index.jsx
+++ app/Views/Home/Home/index.jsx
@@ -1,6 +1,6 @@
 import { getAuth, signOut } from '@firebase/auth';
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { app, auth } from '../../../../configs/firebase.config.mjs';
 import NavBar from '../../../../assets/components/NavBar'
 import { StackActions } from '../../../../configs/views.config.mjs';
@@ -8,36 +8,54 @@ import Categorie from '../../../../assets/components/Categorie';
 
 export default function Home({ navigation }) {
 
+    const user = getAuth(app).currentUser
+
+    if (!user) {
+        
+    }
+
     const [categoria, setCategoria] = useState('Tudo')
+    const [isRefreshing, setIsRefrshing] = useState(false)
 
     const handleCategoriaPress = (categoria) => {
         setCategoria(categoria)
     }
 
-    if (!getAuth(app).currentUser) {
-
+    function handleRefresh() {
+        console.log('Refresh!');
     }
 
     return (
-        <View style={styles.bg}>
-            <NavBar />
-            <View style={styles.container}>
-                <View>
-                    <ScrollView showsHorizontalScrollIndicator={false} bounces={false} alwaysBounceHorizontal={false} bouncesZoom={false} horizontal >
-                        <Categorie text='Tudo' Selecionada={categoria === 'Tudo'} onPress={()=>{handleCategoriaPress('Tudo')}}/>
-                        <Categorie text='Filosóficas' Selecionada={categoria === 'Filosóficas'} onPress={()=>{handleCategoriaPress('Filosóficas')}}/>
-                        <Categorie text='Poemas' Selecionada={categoria === 'Poemas'} onPress={()=>{handleCategoriaPress('Poemas')}}/>
-                        <Categorie text='Acolhedoras' Selecionada={categoria === 'Acolhedoras'} onPress={()=>{handleCategoriaPress('Acolhedoras')}}/>
-                        <Categorie text='Motivacionais' Selecionada={categoria === 'Motivacionais'} onPress={()=>{handleCategoriaPress('Motivacionais')}}/>
-                        <Categorie text='Amor' Selecionada={categoria === 'Amor'} onPress={()=>{handleCategoriaPress('Amor')}}/>
-                        <Categorie text='Amizade' Selecionada={categoria === 'Amizade'} onPress={()=>{handleCategoriaPress('Amizade')}}/>
-                        <Categorie text='Vida' Selecionada={categoria === 'Vida'} onPress={()=>{handleCategoriaPress('Vida')}}/>
-                        <Categorie text='Trabalho' Selecionada={categoria === 'Trabalho'} onPress={()=>{handleCategoriaPress('Trabalho')}}/>
-                        <Categorie text='Espirtualidade' Selecionada={categoria === 'Espirtualidade'} onPress={()=>{handleCategoriaPress('Espirtualidade')}}/>
-                    </ScrollView>
-                </View>
-            </View>
-        </View>
+        <FlatList
+            data={[{}]}
+            style={{ flex: 1, backgroundColor: "#fff" }}
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
+            renderItem={(item) => {
+                return (
+
+                    <View style={styles.bg}>
+                        <NavBar />
+                        <View style={styles.container}>
+                            <View>
+                                <ScrollView showsHorizontalScrollIndicator={false} bounces={false} alwaysBounceHorizontal={false} bouncesZoom={false} horizontal >
+                                    <Categorie text='Tudo' Selecionada={categoria === 'Tudo'} onPress={() => { handleCategoriaPress('Tudo') }} />
+                                    <Categorie text='Filosóficas' Selecionada={categoria === 'Filosóficas'} onPress={() => { handleCategoriaPress('Filosóficas') }} />
+                                    <Categorie text='Poemas' Selecionada={categoria === 'Poemas'} onPress={() => { handleCategoriaPress('Poemas') }} />
+                                    <Categorie text='Acolhedoras' Selecionada={categoria === 'Acolhedoras'} onPress={() => { handleCategoriaPress('Acolhedoras') }} />
+                                    <Categorie text='Motivacionais' Selecionada={categoria === 'Motivacionais'} onPress={() => { handleCategoriaPress('Motivacionais') }} />
+                                    <Categorie text='Amor' Selecionada={categoria === 'Amor'} onPress={() => { handleCategoriaPress('Amor') }} />
+                                    <Categorie text='Amizade' Selecionada={categoria === 'Amizade'} onPress={() => { handleCategoriaPress('Amizade') }} />
+                                    <Categorie text='Vida' Selecionada={categoria === 'Vida'} onPress={() => { handleCategoriaPress('Vida') }} />
+                                    <Categorie text='Trabalho' Selecionada={categoria === 'Trabalho'} onPress={() => { handleCategoriaPress('Trabalho') }} />
+                                    <Categorie text='Espirtualidade' Selecionada={categoria === 'Espirtualidade'} onPress={() => { handleCategoriaPress('Espirtualidade') }} />
+                                </ScrollView>
+                            </View>
+                        </View>
+                    </View>
+                )
+            }}
+        />
     );
 }
 
@@ -45,7 +63,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#ffffff",
         paddingTop: 10,
-        paddingHorizontal:15
+        paddingHorizontal: 15
     },
     bg: {
         gap: 15,
