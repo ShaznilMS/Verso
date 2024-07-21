c1c28791b5412bf20466d4997aef70f8338c14d5
Antonio Cossa <antoniovcossa16@gmail.com>
2024-07-21 02:55:06 +0200

    update app/Views/Home/Publication/Components/ActionBar/index.jsx and app/Views/Home/Publication/Components/Card/index.jsx

--- app/Views/Home/Publication/Components/Card/index.jsx
+++ app/Views/Home/Publication/Components/Card/index.jsx
@@ -5,10 +5,13 @@ import ActionBar from '../ActionBar'
 
 const Card = (props) => {
     return (
-        <TouchableOpacity activeOpacity={1} style={{borderTopWidth:5,borderColor:'#d4d4d43d'}} onPress={props.onPress}>
+        <TouchableOpacity activeOpacity={1} style={{ borderTopWidth: 5, borderColor: '#d4d4d43d' }} onPress={props.onPress}>
             <User time='20/07/24' user='Antonio Cossa' />
             <View style={styles.content}>
-                <Text style={{ textAlign: 'center', fontWeight:500 }}>{props.content}Prefiro encarar a derrota com plena consciência de sua inevitabilidade do que me iludir com uma falsa expectativa de vitória</Text>
+                <View>
+                    <Text style={{ textAlign: 'center', fontWeight: 500 }}>{props.content}Prefiro encarar a derrota com plena consciência de sua inevitabilidade do que me iludir com uma falsa expectativa de vitória</Text>
+                    <Text style={{ textAlign: 'center', fontWeight: 700, top: 30 }}>{props.cit}Raimundo Chitava</Text>
+                </View>
             </View>
             <ActionBar likes='50' coments='3' shares='23' />
         </TouchableOpacity>
@@ -18,12 +21,13 @@ const Card = (props) => {
 export default Card
 
 const styles = StyleSheet.create({
-    content:{
-        borderTopWidth:1,
-        borderBottomWidth:1,
-        borderColor:'#f5f5f5a4',
-        width:'100%',
-        paddingVertical:50,
-        backgroundColor:'#f5f5f5a4'
+    content: {
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#f5f5f5a4',
+        width: '100%',
+        paddingVertical: 50,
+        backgroundColor: '#f5f5f5a4',
+        paddingHorizontal: 10
     }
 })
