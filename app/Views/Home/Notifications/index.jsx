d66e498928d3933404b423bf50565af4542b7f66
Antonio Cossa <antoniovcossa16@gmail.com>
2024-07-21 04:48:03 +0200

    update 1 file and delete 3 files

--- app/Views/Home/Notifications/index.jsx
+++ app/Views/Home/Notifications/index.jsx
@@ -2,14 +2,6 @@ import { getAuth } from '@firebase/auth';
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { app } from '../../../../configs/firebase.config.mjs';
-import NavBar from '../../../../assets/components/NavBar'
-import FollowCard from './Componentes/NotificationCards/FollowCard';
-import User from '../Publication/Components/User';
-import Card from '../Publication/Components/Card';
-import Details from '../Publication/Details';
-// import LikeCard from './Componentes/NotificationCards/LikeCard';
-// import ShareCard from './Componentes/NotificationCards/ShareCard';
-// import NewPubCard from './Componentes/NotificationCards/NewPubCard';
 
 export default function Notifications({ navigation }) {
 
@@ -26,8 +18,6 @@ export default function Notifications({ navigation }) {
     return (
 
         <View style={styles.bg}>
-
-<Details/>
 
             <FlatList
                 data={[{}]}
