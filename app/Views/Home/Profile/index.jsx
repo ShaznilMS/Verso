3d9ea8c209ed5cd722630e0f99102c81d3b40b94
ShaznilMS <shaznilmussagyoficial@gmail.com>
2024-07-22 17:16:48 +0100

    update app/Views/Home/Notifications/index.jsx and app/Views/Home/Profile/index.jsx

--- app/Views/Home/Profile/index.jsx
+++ app/Views/Home/Profile/index.jsx
@@ -1,6 +1,6 @@
 import { getAuth, signOut } from '@firebase/auth';
-import React, { useState } from 'react';
-import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Image, Modal, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { app, auth } from '../../../../configs/firebase.config.mjs';
 import { Link } from 'expo-router';
 import { StackActions } from '@react-navigation/native';
@@ -8,11 +8,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faArrowRightFromBracket, faAt, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { StatusBar } from 'expo-status-bar';
 import { faCalendar, faUser } from '@fortawesome/free-regular-svg-icons';
+import { get, getDatabase, ref } from 'firebase/database';
+import sha256 from 'sha256';
 
 const User_Image = require('./../../../../assets/USER/USER_PROFILE.jpg')
 
 export default function Profile({ navigation }) {
 
+    const [UserInformation, setUserInformation] = useState()
+    const [isStarted, setIsStarted] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        if (!isStarted) {
+            setIsStarted(true)
+            GetUser()
+        }
+    })
 
     if (!getAuth(app).currentUser) {
         navigation.reset({
@@ -38,6 +50,24 @@ export default function Profile({ navigation }) {
         }
     }
 
+    function getUserId() {
+        return sha256(getAuth(app).currentUser.email)
+    }
+
+    function GetUser() {
+        const db = getDatabase(app)
+        const reference = ref(db, 'users/' + getUserId())
+
+        get(reference)
+            .then((value) => {
+                setUserInformation(value.val())
+            })
+            .finally(() => {
+                console.log('Finally');
+                setIsLoading(false)
+            })
+    }
+
     function InformationField(props) {
         return (
             <View
@@ -46,14 +76,14 @@ export default function Profile({ navigation }) {
                     width: '100%',
                     justifyContent: 'space-between',
                     marginVertical: 7,
-                    alignItems:'center'
+                    alignItems: 'center'
                 }}
             >
                 <View
                     style={{
                         flexDirection: 'row',
                         gap: 10,
-                        alignItems:'center'
+                        alignItems: 'center'
                     }}
                 >
                     <FontAwesomeIcon size={16} color='#333' icon={props.icon} />
@@ -81,7 +111,7 @@ export default function Profile({ navigation }) {
             <View style={{
                 paddingHorizontal: 15,
                 borderRadius: 50,
-                paddingVertical:3,
+                paddingVertical: 3,
                 gap: 8,
                 justifyContent: 'center',
                 alignItems: 'center',
@@ -105,7 +135,7 @@ export default function Profile({ navigation }) {
                     style={{
                         fontSize: 18,
                         fontWeight: '700',
-                        color:'#aaa'
+                        color: '#aaa'
                     }}
                 >{props.num}</Text>
 
@@ -113,205 +143,229 @@ export default function Profile({ navigation }) {
         )
     }
 
-    return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor='#111111' style='light' />
-            <View style={{
-                backgroundColor: "#111111",
-                height: 130,
-                borderBottomRightRadius: 20,
-                borderBottomLeftRadius: 20,
-                padding: 10,
-                alignItems: 'center',
-                gap: 20
-            }}>
+    if (isLoading) {
+        return (
+            <Modal>
                 <View
                     style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        width: '100%'
-                    }}
-                >
-                    <View style={{
-                        width: 50,
-                        height: 50,
+                        flex: 1,
                         justifyContent: 'center',
                         alignItems: 'center'
-                    }}>
-
-                    </View>
-                    <Text style={{
-                        color: "#ffffff",
-                        fontSize: 20,
-                        fontWeight:'bold'
-                    }}>Raimundo Chitava</Text>
-
-                    <TouchableOpacity activeOpacity={.8} onPress={Sair}>
+                    }}
+                >
+                    <ActivityIndicator color={"#333"} size={40}></ActivityIndicator>
+                </View>
+            </Modal>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <StatusBar backgroundColor='#111111' style='light' />
+                <View style={{
+                    backgroundColor: "#111111",
+                    height: 130,
+                    borderBottomRightRadius: 20,
+                    borderBottomLeftRadius: 20,
+                    padding: 10,
+                    alignItems: 'center',
+                    gap: 20
+                }}>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '100%'
+                        }}
+                    >
                         <View style={{
                             width: 50,
                             height: 50,
-                            borderRadius: 25,
                             justifyContent: 'center',
                             alignItems: 'center'
                         }}>
-                            <FontAwesomeIcon size={20} color='#ffffff' icon={faArrowRightFromBracket} />
+
                         </View>
-                    </TouchableOpacity>
-                </View>
+                        <Text style={{
+                            color: "#ffffff",
+                            fontSize: 20,
+                            fontWeight: 'bold'
+                        }}>{UserInformation.Name}</Text>
 
-                <Image source={User_Image} style={{
-                    width: 100,
-                    height: 100,
-                    borderRadius: 50,
-                    borderWidth: 3,
-                    borderColor: "#ffffff"
-                }} />
-            </View>
-
-            <View
-                style={{
-                    alignItems: 'center',
-                    paddingTop: 50,
-                    gap: 5
-                }}
-            >
-                <Text style={{ fontSize: 18, fontStyle: 'italic', fontWeight: '700', letterSpacing: 1 }}>@mundinho340</Text>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        gap: 8,
-                        alignItems: 'center'
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: 18,
-                            color: "#528599",
-                            fontWeight: '700'
-                        }}
-                    >
-                        MOZAMBIQUE
-                    </Text>
-                    <View
-                        style={{
-                            width: 2,
-                            height: '100%',
-                            backgroundColor: "#000000"
-                        }}>
-
+                        <TouchableOpacity activeOpacity={.8} onPress={Sair}>
+                            <View style={{
+                                width: 50,
+                                height: 50,
+                                borderRadius: 25,
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <FontAwesomeIcon size={20} color='#ffffff' icon={faArrowRightFromBracket} />
+                            </View>
+                        </TouchableOpacity>
                     </View>
-                    <Text>
-                        Joined March 2023
-                    </Text>
+
+                    <TouchableWithoutFeedback
+                        onPress={GetUser}
+                    >
+                        <Image source={User_Image} style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: 50,
+                            borderWidth: 3,
+                            borderColor: "#ffffff"
+                        }} />
+                    </TouchableWithoutFeedback>
                 </View>
 
                 <View
                     style={{
-                        flexDirection: 'row',
                         alignItems: 'center',
-                        gap: 8,
-                        padding: 6,
-                        paddingHorizontal: 14,
-                        borderWidth: 1,
-                        borderColor: "#aaa",
-                        borderRadius: 10,
-                        marginTop: 10
+                        paddingTop: 50,
+                        gap: 5
                     }}
                 >
-                    <FontAwesomeIcon icon={faUser} size={18} />
-                    <Text
-                        style={{
-                            fontWeight: '700'
-                        }}
-                    >
-                        Followers
-                    </Text>
-                    <Text>
-                        •
-                    </Text>
-                    <Text
-                        style={{
-                            fontWeight: '700'
-                        }}
-                    >
-                        1987
-
-                    </Text>
-                </View>
-
-                <Text
-                    style={{
-                        fontSize: 14,
-                        color: "#aaa",
-                        marginHorizontal: 25,
-                        lineHeight: 24,
-                        fontWeight: '400',
-                        textAlign: 'center',
-                        marginTop: 6
-                    }}
-                >
-                    Raimundo Chitava is a dedicated backend developer known for his exceptional skills in building robust, scalable, and efficient backend systems.
-                </Text>
-
-                <View
-                    style={{
-                        width: '100%',
-                        gap: 4
-                    }}
-                >
-
+                    <Text style={{ fontSize: 18, fontStyle: 'italic', fontWeight: '700', letterSpacing: 1 }}>@{UserInformation.Email.split('@')[0]}</Text>
                     <View
                         style={{
-                            gap: 4,
-                            paddingHorizontal: 20,
-                            paddingVertical: 15,
-                            borderTopWidth:1,
-                            borderBottomWidth:1
+                            flexDirection: 'row',
+                            gap: 8,
+                            alignItems: 'center'
                         }}
                     >
                         <Text
                             style={{
-                                fontWeight: '700',
-                                fontSize: 18
+                                fontSize: 18,
+                                color: "#528599",
+                                fontWeight: '700'
                             }}
                         >
-                            Information
+                            {UserInformation.Country.toUpperCase()}
                         </Text>
-                        <InformationField icon={faUser} name="Name" text="Raimundo Chitava" />
-                        <InformationField icon={faAt} name="Email" text="mundinho340@gmail.com" />
-                        <InformationField icon={faPhone} name="Phone" text="+258 851935325" />
-                        <InformationField icon={faCalendar} name="Joined" text="March" />
+                        <View
+                            style={{
+                                width: 2,
+                                height: '100%',
+                                backgroundColor: "#000000"
+                            }}>
+
+                        </View>
+                        <Text>
+                            Joined {UserInformation.Joined}
+                        </Text>
                     </View>
 
-                    <ScrollView
-                        style={{
-                            width: '100%',
-                            marginTop: 10
-                        }}
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
+                    <TouchableWithoutFeedback
+                        onPress={() => { console.log(UserInformation.Name) }}
                     >
                         <View
+                            style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                gap: 8,
+                                padding: 6,
+                                paddingHorizontal: 14,
+                                borderWidth: 1,
+                                borderColor: "#aaa",
+                                borderRadius: 10,
+                                marginTop: 10
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faUser} size={18} />
+                            <Text
+                                style={{
+                                    fontWeight: '700'
+                                }}
+                            >
+                                Followers
+                            </Text>
+                            <Text>
+                                •
+                            </Text>
+                            <Text
+                                style={{
+                                    fontWeight: '700'
+                                }}
+                            >
+                                {UserInformation.Followers}
+
+                            </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+
+                    <Text
+                        style={{
+                            fontSize: 14,
+                            color: "#aaa",
+                            marginHorizontal: 25,
+                            lineHeight: 24,
+                            fontWeight: '400',
+                            textAlign: 'center',
+                            marginTop: 6
+                        }}
+                    >
+                        {UserInformation.Biography}
+                    </Text>
+
+                    <View
+                        style={{
+                            width: '100%',
+                            gap: 4
+                        }}
+                    >
+
+                        <View
+                            style={{
+                                gap: 4,
+                                paddingHorizontal: 20,
+                                paddingVertical: 15,
+                                borderTopWidth: 1,
+                                borderBottomWidth: 1
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    fontWeight: '700',
+                                    fontSize: 18
+                                }}
+                            >
+                                Information
+                            </Text>
+                            <InformationField icon={faUser} name="Name" text={UserInformation.Name} />
+                            <InformationField icon={faAt} name="Email" text={UserInformation.Email} />
+                            <InformationField icon={faPhone} name="Phone" text={UserInformation.Phone_Number} />
+                            <InformationField icon={faCalendar} name="Joined" text={UserInformation.Joined} />
+                        </View>
+
+                        <ScrollView
+                            style={{
+                                width: '100%',
+                                marginTop: 10
+                            }}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                        >
+                            {/* <View
                             style={{
                                 width: '100%',
                                 flexDirection: 'row',
                                 paddingHorizontal: 20,
                                 paddingVertical: 5,
                                 gap: 10
-                            }}
-                        >
-                            <RoundCategories name="Poemas" num={13} />
-                            <RoundCategories name="Vidas" num={4} />
-                            <RoundCategories name="Amizade" num={6} />
-                            <RoundCategories name="Acolhedoras" num={9} />
-                            <RoundCategories name="Amor" num={10} />
-                        </View>
-                    </ScrollView>
+                                }}
+                                >
+                                <RoundCategories name="Poemas" num={13} />
+                                <RoundCategories name="Vidas" num={4} />
+                                <RoundCategories name="Amizade" num={6} />
+                                <RoundCategories name="Acolhedoras" num={9} />
+                                <RoundCategories name="Amor" num={10} />
+                                </View> */}
+                        </ScrollView>
+                    </View>
                 </View>
             </View>
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
